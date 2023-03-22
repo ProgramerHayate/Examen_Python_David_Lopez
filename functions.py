2551732471db850a85ab5c473924b1b4069e1d81
hayate <davidlopez.gandia@gmail.com>
2023-03-22 10:30:36 +0100

    commit_2

--- functions.py
+++ functions.py
@@ -1,6 +1,12 @@
-import xlwt
-from xlwt import Workbook
+import csv
 
 
 def read_data(fich):
-    
+    dic1 = {}
+    with open(fich, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print(row)
+
+
+    return dic1
---
+++ main.py
@@ -0,0 +1,4 @@
+import functions
+
+
+functions.read_data('winequality.csv')
