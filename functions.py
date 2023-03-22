5f893eb61a5accd58072020a1213b36f3c52bc80
hayate <davidlopez.gandia@gmail.com>
2023-03-22 10:45:54 +0100

    commit_3

--- functions.py
+++ functions.py
@@ -3,10 +3,21 @@ import csv
 
 def read_data(fich):
     dic1 = {}
+    cont = 0
+    atributos = list()
     with open(fich, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            print(row)
-
+            if cont == 0:
+                atributos = row
+            else:
+                dicaux = {}
+                iterador = 0
+                for i in row:
+                    
+                    dicaux[atributos[iterador]] = i
+                dic1["dato"+ str(cont)] = dicaux
+            cont = cont + 1
+    print(dic1["dato1"])
 
     return dic1
